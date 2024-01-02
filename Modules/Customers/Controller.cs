64bba8f083fc984a2b11cd3101dab6ab17c816c8
Vicham <Vicham@gmail.com>
2024-01-02 14:41:58 +0700

    commnet

--- Modules/Customers/Controller.cs
+++ Modules/Customers/Controller.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using NJsonSchema.Annotations;
 using WebApi.Core;
 using WebApi.Modules.Comstomers;
 
@@ -8,7 +9,6 @@ namespace WebApi.Modules.Customers;
 
 public class CustomersController : MyController
 {
-    // private readonly ICloudStorageSingletonService _service;
     private readonly IMapper _mapper;
     private readonly ICustomersRepository _repository;
 
@@ -23,19 +23,37 @@ public class CustomersController : MyController
     }
 
     [HttpGet]
-    public IActionResult GetAllCustomers()
+    public IActionResult Gets([FromQuery] PaginationRequest item)
     {
-        var customer = _repository.GetAll();
-        var CustomersModel = _mapper.ProjectTo<GetCustomersResponse>(customer);
-
-        return Ok(CustomersModel);
+        var iQueryable = _repository.FindBy(e => e.DeletedAt == null); //find not deleted id of cumstomers  
+        //check for sort by firstname
+        if (item.Sort == "fn")
+        {
+            iQueryable = iQueryable.OrderBy(e => e.FirstName);
+        }
+        //check for sort by lastname
+        else if (item.Sort == "ln")
+        {
+            iQueryable = iQueryable.OrderBy(e => e.LastName);
+        }
+        //check for sort by no order
+        else
+        {
+            iQueryable = iQueryable.OrderBy(e => e.DeletedAt);
+        }
+        var afterfiler = iQueryable //filter the deleted id
+            .Skip((item.CurrentPage - 1) * item.PageSize) //sort item by 10 of one page 
+            .Take(item.PageSize); //get not deleted item that put in pagesize
+        var results = _mapper.ProjectTo<GetCustomersResponse>(afterfiler).ToList();//then filter item to list
+        return Ok(results);//return list of result
     }
+
     [HttpGet("{id:guid}")]
     public IActionResult GetById(Guid id)
     {
-        var customersId = _repository.GetSingle(e => e.Id == id && e.DeletedAt == null);
-        var customersIdModel = _mapper.Map<GetCustomersResponse>(customersId);
-        if (customersId == null)
+        var customersId = _repository.GetSingle(e => e.Id == id && e.DeletedAt == null);//go to database to find specifix id 
+        var customersIdModel = _mapper.Map<GetCustomersResponse>(customersId);//go to overall model model in db and point to only id model 
+        if (customersId == null)//check id in null or not 
         {
             return NotFound("Not Found");
         }
@@ -48,20 +66,21 @@ public class CustomersController : MyController
         {
             return BadRequest("invalid");
         }
-        var createdCustomers = _mapper.Map<Customers>(item);
-        _repository.Add(createdCustomers);
-        _repository.Commit();
+        var createdCustomers = _mapper.Map<Customers>(item);//map for model to push information in correct column
+        _repository.Add(createdCustomers);//add the detail column of customer 
+        _repository.Commit();//add cumstomer information to db
 
         return Ok();
     }
     [HttpPut("{id:guid}")]
     public IActionResult UpdateCustomers([FromBody] UpdateCustomersRequest request, Guid id)
     {
-        var item = _repository.GetSingle(e => e.Id == id);
+        var item = _repository.GetSingle(e => e.Id == id);//go to db and find the id of customer
         if (item == null)
         {
             return NotFound("Not found");
         }
+        //check Firstname and Lastname if not empty or empty
         if (request.FirstName != "")
         {
             request.FirstName = item.FirstName;
@@ -70,23 +89,26 @@ public class CustomersController : MyController
         {
             request.LastName = item.LastName;
         }
-        _mapper.Map(request, item);
-        _repository.Update(item);
-        _repository.Commit();
+        _mapper.Map(request, item);//map firstname or lastname in change
+        _repository.Update(item);//add new change lastname or firstname t
+        _repository.Commit();//push new change to db
         return Ok();
     }
-    [HttpDelete("{id:guid}")]
 
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteById(Guid id)
     {
-        var existed = _repository.Existed(e => e.Id == id);
-
+        var existed = _repository.Existed(e => e.Id == id);//check out id in db
+        //find id exite or not
         if (!existed)
         {
             return NotFound("Not Found");
         }
+        //if exit go to db and get id 
         var item = _repository.GetSingle(e => e.Id == id);
+        //update db in deleteat column
         item!.DeletedAt = DateTime.UtcNow;
+        //pust data deletedat to db
         _repository.Update(item!);
         _repository.Commit();
         return Ok();
